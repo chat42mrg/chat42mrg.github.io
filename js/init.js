58723cfccba213e2e33d3e8c4cb8677e5058608d
chat42mrg <81402837+chat42mrg@users.noreply.github.com>
2021-03-25 22:57:53 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBbilQcNThEaldmjrjrDuWUGVItv1CsRG0",
+    authDomain: "chat42am.firebaseapp.com",
+    projectId: "chat42am",
+    storageBucket: "chat42am.appspot.com",
+    messagingSenderId: "689527327261",
+    appId: "1:689527327261:web:1888ba57d880bcce8154f4",
+    measurementId: "G-94CWYPGYTF"
+    });
