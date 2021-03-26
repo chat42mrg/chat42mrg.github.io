09aa308db5bed903e68af0cf3ae825eabb9f1788
chat42mrg <81402837+chat42mrg@users.noreply.github.com>
2021-03-25 23:22:26 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBbilQcNThEaldmjrjrDuWUGVItv1CsRG0",
-    authDomain: "chat42am.firebaseapp.com",
-    projectId: "chat42am",
-    storageBucket: "chat42am.appspot.com",
-    messagingSenderId: "689527327261",
-    appId: "1:689527327261:web:1888ba57d880bcce8154f4",
-    measurementId: "G-94CWYPGYTF"
+    apiKey: "AIzaSyCEmClCCjOu-1_44bFiFuyynLSIasVpZ8g",
+    authDomain: "chat42mgr.firebaseapp.com",
+    projectId: "chat42mgr",
+    storageBucket: "chat42mgr.appspot.com",
+    messagingSenderId: "290697275100",
+    appId: "1:290697275100:web:5535148913de5a80cfb619",
+    measurementId: "G-CVN8YTT1E3"
     });
